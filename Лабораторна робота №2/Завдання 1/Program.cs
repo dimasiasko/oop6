1afc41258e89596cc7aa14624ee37cd9c1105b91
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2020-09-24 11:06:57 +0300

    first commit

---
+++ Лабораторна робота №2/Завдання 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Завдання_1
+{
+    internal class Output
+    {
+        public virtual void Outputer(string line)
+        {
+            Console.WriteLine($"{line} и его тип = {line.GetType()}");
+        }
+    }
+    internal class OutputGreen : Output
+    {
+        public override void Outputer(string line)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            base.Outputer($"{line} 2");
+        }
+    }
+    internal class OutputRed : Output
+    {
+        public override void Outputer(string line)
+        {
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            base.Outputer($"{line} 3");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string lineMain = "Наш вывод";
+            
+            var arr = new Output[3];
+            arr[0] = new Output();
+            arr[1] = new OutputGreen();
+            arr[2] = new OutputRed();
+
+            foreach (Output obj in arr)
+            {
+                obj.Outputer(lineMain);
+            }
+
+            string newline = ("Использование привидения типов");
+            ((OutputRed)arr[2]).Outputer(newline);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Лабораторна робота №2/Завдання 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Завдання_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
