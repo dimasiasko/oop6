6ad9018886ce3ef1dc7c4cf2a7a791f68383eb62
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2020-09-26 23:24:02 +0300

    3 commit

---
+++ Лабораторна робота №2/Завдання 3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Завдання_3
+{
+    class BaseClass
+    {
+        public int money = 2000;
+        public string currencyName = "Ukrainian Dollar";
+        public double exchangeValue = 28.41;
+
+        public virtual void Show()
+        {
+            Console.WriteLine(money);
+            Console.WriteLine(currencyName);
+            Console.WriteLine(exchangeValue);
+        }
+    }
+    class Money:BaseClass
+    {
+        public override void Show()
+        {
+            Console.WriteLine(money);
+        }
+    }
+    class Currency:BaseClass
+    {
+        public override void Show()
+        {
+            Console.WriteLine(currencyName);
+        }
+    }
+    class Value : BaseClass
+    {
+        public override void Show()
+        {
+            Console.WriteLine(exchangeValue);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new BaseClass[3];          
+            array[0] = new Money();
+            array[1] = new Currency();
+            array[2] = new Value();
+
+            foreach (BaseClass obj in array)
+            {
+                obj.Show();
+            }
+
+        }
+    }
+}
