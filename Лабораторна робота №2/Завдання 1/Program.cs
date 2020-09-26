a971af72ccc8c2b8dac1af0c30d7e409a5ed2d3d
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2020-09-26 23:08:18 +0300

    2commit

--- Лабораторна робота №2/Завдання 1/Program.cs
+++ Лабораторна робота №2/Завдання 1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace Завдання_1
 {
@@ -6,7 +7,8 @@ namespace Завдання_1
     {
         public virtual void Outputer(string line)
         {
-            Console.WriteLine($"{line} и его тип = {line.GetType()}");
+            Debug.WriteLine(($"{line} и его тип = {line.GetType()}"));
+            
         }
     }
     internal class OutputGreen : Output
