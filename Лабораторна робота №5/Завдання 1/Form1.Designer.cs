97aeea10edff96f23860619b8646d367b3782cc3
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2020-10-04 00:17:32 +0300

    first commit

---
+++ Лабораторна робота №5/Завдання 1/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Завдання_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.InputLentght = new System.Windows.Forms.TextBox();
+            this.InputY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.InputX = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 75);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите Длину Стороны квадрата";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InputLentght
+            // 
+            this.InputLentght.Location = new System.Drawing.Point(68, 87);
+            this.InputLentght.Name = "InputLentght";
+            this.InputLentght.Size = new System.Drawing.Size(78, 20);
+            this.InputLentght.TabIndex = 1;
+            // 
+            // InputY
+            // 
+            this.InputY.Location = new System.Drawing.Point(122, 211);
+            this.InputY.Name = "InputY";
+            this.InputY.Size = new System.Drawing.Size(77, 20);
+            this.InputY.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label2.Location = new System.Drawing.Point(12, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 75);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Введите Координаты Х и Y";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // InputX
+            // 
+            this.InputX.Location = new System.Drawing.Point(15, 211);
+            this.InputX.Name = "InputX";
+            this.InputX.Size = new System.Drawing.Size(77, 20);
+            this.InputX.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.Location = new System.Drawing.Point(218, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(570, 426);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(15, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 63);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(12, 384);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 23);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(12, 418);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 23);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "label4";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Location = new System.Drawing.Point(15, 327);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(159, 36);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "Нажмите чтобы поворачивать квадрат";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.InputX);
+            this.Controls.Add(this.InputY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.InputLentght);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox InputLentght;
+        private System.Windows.Forms.TextBox InputY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox InputX;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Лабораторна робота №5/Завдання 1/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Завдання_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+                    
+        }
+
+        public int X;
+        public int Y;
+        public int lenght;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                X = int.Parse(InputX.Text);
+                Y = int.Parse(InputY.Text);
+                lenght = int.Parse(InputLentght.Text);
+
+                Bitmap bmp = new Bitmap(500, 500);
+                Graphics g = Graphics.FromImage(bmp);
+                Color color = Color.Black;
+                Rectangle rectangle = new Rectangle(X, Y, lenght, lenght);
+                g.DrawRectangle(new Pen(color), rectangle);
+                pictureBox1.Image = bmp;
+                label3.Text = $"Координаты по Х = {X}";
+                label4.Text = $"Координаты по Х = {Y}";
+            }
+
+            catch (Exception)
+            {
+                MessageBox.Show("Введите целое число!");
+                InputLentght.Text = string.Empty;
+                InputX.Text = string.Empty;
+                InputY.Text = string.Empty;
+            }
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int currentKey = e.KeyValue;
+            if (checkBox1.Checked == true)
+            {
+                switch (currentKey)
+                {
+                    case 37:
+                        pictureBox1.Location = new Point(pictureBox1.Location.X - lenght, pictureBox1.Location.Y);
+                        X += -lenght;                      
+                        break;
+                    case 38:
+                        pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - lenght);
+                        Y += -lenght;
+                        break;
+                    case 39:
+                        pictureBox1.Location = new Point(pictureBox1.Location.X + lenght, pictureBox1.Location.Y);
+                        X += lenght;
+                        break;
+                    case 40:
+                        pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + lenght);
+                        Y += lenght;
+                        break;
+                }               
+            }
+            else
+            {
+                switch (currentKey)
+                {
+                    case 37:
+                        pictureBox1.Location = new Point(pictureBox1.Location.X - 2, pictureBox1.Location.Y);
+                        X += -2;
+                        break;
+                    case 38:
+                        pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 2);
+                        Y += -2;
+                        break;
+                    case 39:
+                        pictureBox1.Location = new Point(pictureBox1.Location.X + 2, pictureBox1.Location.Y);
+                        X += 2;
+                        break;
+                    case 40:
+                        pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 2);
+                        Y += 2;
+                        break;
+                }
+            }
+
+
+
+            label3.Text = $"Координаты по Х = {X}";
+            label4.Text = $"Координаты по Y = {Y}";
+        }
+
+        
+    }
+}
+
