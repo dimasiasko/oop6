cb07ef3ed8464112866eadbb47e0dd1e474bd1c7
dimasiasko <70683494+dimasiasko@users.noreply.github.com>
2020-10-15 13:37:42 +0300

    commit

---
+++ MusicPlayerv1/MusicAppv1/Form1.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MusicAppv1
+{
+    partial class MusicPlayerApp
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MusicPlayerApp));
+            this.TopPanel = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.IblLogo = new System.Windows.Forms.Label();
+            this.listBoxSongs = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnSelectSongs = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayerMusic = new AxWMPLib.AxWindowsMediaPlayer();
+            this.lblFooter = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TopPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerMusic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TopPanel
+            // 
+            this.TopPanel.BackColor = System.Drawing.Color.Teal;
+            this.TopPanel.Controls.Add(this.pictureBox1);
+            this.TopPanel.Controls.Add(this.IblLogo);
+            this.TopPanel.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.TopPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopPanel.Location = new System.Drawing.Point(0, 0);
+            this.TopPanel.Name = "TopPanel";
+            this.TopPanel.Size = new System.Drawing.Size(289, 51);
+            this.TopPanel.TabIndex = 0;
+            this.TopPanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseDown);
+            this.TopPanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopPanel_MouseMove);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DarkCyan;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(260, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(26, 24);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // IblLogo
+            // 
+            this.IblLogo.AutoSize = true;
+            this.IblLogo.Font = new System.Drawing.Font("Algerian", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.IblLogo.Location = new System.Drawing.Point(12, 9);
+            this.IblLogo.Name = "IblLogo";
+            this.IblLogo.Size = new System.Drawing.Size(96, 15);
+            this.IblLogo.TabIndex = 0;
+            this.IblLogo.Text = "Music Player";
+            // 
+            // listBoxSongs
+            // 
+            this.listBoxSongs.AllowDrop = true;
+            this.listBoxSongs.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBoxSongs.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBoxSongs.FormattingEnabled = true;
+            this.listBoxSongs.ItemHeight = 16;
+            this.listBoxSongs.Location = new System.Drawing.Point(15, 179);
+            this.listBoxSongs.Name = "listBoxSongs";
+            this.listBoxSongs.Size = new System.Drawing.Size(255, 260);
+            this.listBoxSongs.TabIndex = 1;
+            this.listBoxSongs.SelectedIndexChanged += new System.EventHandler(this.listBoxSongs_SelectedIndexChanged);
+            this.listBoxSongs.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxSongs_DragEnter);
+            this.listBoxSongs.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBoxSongs_MouseDown);
+            this.listBoxSongs.MouseUp += new System.Windows.Forms.MouseEventHandler(this.listBoxSongs_MouseUp);
+            this.listBoxSongs.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.listBoxSongs_PreviewKeyDown);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnSelectSongs
+            // 
+            this.btnSelectSongs.BackColor = System.Drawing.Color.OrangeRed;
+            this.btnSelectSongs.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSelectSongs.FlatAppearance.MouseDownBackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnSelectSongs.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Tomato;
+            this.btnSelectSongs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSelectSongs.ForeColor = System.Drawing.Color.White;
+            this.btnSelectSongs.Location = new System.Drawing.Point(15, 454);
+            this.btnSelectSongs.Name = "btnSelectSongs";
+            this.btnSelectSongs.Size = new System.Drawing.Size(255, 40);
+            this.btnSelectSongs.TabIndex = 2;
+            this.btnSelectSongs.Text = "Выбрать файлы";
+            this.btnSelectSongs.UseVisualStyleBackColor = false;
+            this.btnSelectSongs.Click += new System.EventHandler(this.btnSelectSongs_Click);
+            // 
+            // axWindowsMediaPlayerMusic
+            // 
+            this.axWindowsMediaPlayerMusic.Enabled = true;
+            this.axWindowsMediaPlayerMusic.Location = new System.Drawing.Point(0, 39);
+            this.axWindowsMediaPlayerMusic.Name = "axWindowsMediaPlayerMusic";
+            this.axWindowsMediaPlayerMusic.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayerMusic.OcxState")));
+            this.axWindowsMediaPlayerMusic.Size = new System.Drawing.Size(289, 134);
+            this.axWindowsMediaPlayerMusic.TabIndex = 3;
+            // 
+            // lblFooter
+            // 
+            this.lblFooter.AutoSize = true;
+            this.lblFooter.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblFooter.Location = new System.Drawing.Point(57, 504);
+            this.lblFooter.Name = "lblFooter";
+            this.lblFooter.Size = new System.Drawing.Size(147, 13);
+            this.lblFooter.TabIndex = 4;
+            this.lblFooter.Text = "Developed by: Dmytro Siasko";
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.BackColor = System.Drawing.Color.CadetBlue;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(15, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 260);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Нажмите кнопку \"Выбрать файлы\"";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.DragEnter += new System.Windows.Forms.DragEventHandler(this.label1_DragEnter);
+            this.label1.DoubleClick += new System.EventHandler(this.label1_DoubleClick);
+            // 
+            // MusicPlayerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(289, 526);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblFooter);
+            this.Controls.Add(this.axWindowsMediaPlayerMusic);
+            this.Controls.Add(this.btnSelectSongs);
+            this.Controls.Add(this.listBoxSongs);
+            this.Controls.Add(this.TopPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MusicPlayerApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Music Player App";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MusicPlayerApp_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MusicPlayerApp_MouseMove);
+            this.TopPanel.ResumeLayout(false);
+            this.TopPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayerMusic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TopPanel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label IblLogo;
+        private System.Windows.Forms.ListBox listBoxSongs;
+        private System.Windows.Forms.Button btnSelectSongs;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayerMusic;
+        private System.Windows.Forms.Label lblFooter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ MusicPlayerv1/MusicAppv1/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Schema;
+
+namespace MusicAppv1
+{
+    public partial class MusicPlayerApp : Form
+    {
+        public MusicPlayerApp()
+        {
+            InitializeComponent();
+            listBoxSongs.Text = "";
+        }
+        //Создаем глобальные переменные массива строкового типа, для сохранения названий треков и пути к ним
+        String[] files;
+        String[] paths;
+        
+        private void btnSelectSongs_Click(object sender, EventArgs e)
+        {
+            // код для кнопки выбора треков
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            // для выбора нескольких файлов
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                
+                files = openFileDialog.SafeFileNames; // Сохраняет имена файлов в массив 
+                paths = openFileDialog.FileNames; // Сохраняет пути к трекам в массив
+
+                label1.Hide(); // скрывает текст
+
+                // Отобразить имена треков в листбоксе
+                for (int i = 0; i < files.Length; i++)
+                {
+                    listBoxSongs.Items.Add(files[i]); // отображаем треки в листбоксе
+                }
+            }
+            else if (openFileDialog.ShowDialog() ==  DialogResult.Cancel)
+            {
+                MessageBox.Show("Выберите файлы для проигрывания!");
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            //Код, который закрывает программу
+            Application.Exit(); 
+        }
+
+        private void listBoxSongs_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // код который проигрывает музыку
+           
+            axWindowsMediaPlayerMusic.URL = paths[listBoxSongs.SelectedIndex];
+            
+                  
+            
+        }
+
+        private void listBoxSongs_DragEnter(object sender, DragEventArgs e)
+        {
+            
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+              e.Effect = DragDropEffects.Copy;        // эффект перетаскивания файлов       
+            } 
+        }
+        private void label1_DragEnter(object sender, DragEventArgs e)
+        {
+            label1.Hide();
+            btnSelectSongs_Click(sender, e); // при перетаскивании файлов вызывает окно выбора файлов
+        }
+
+        private void label1_DoubleClick(object sender, EventArgs e)
+        {
+            btnSelectSongs_Click(sender, e); // двойной клик на надпись вызывает выбор файлов
+        }
+
+
+        // 4  следующих метода которые реализируют перемещение окна
+        // при удерживании кнопки мыши на панеле 
+
+        Point lastPoint;
+        private void TopPanel_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void TopPanel_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void MusicPlayerApp_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+            }
+        }
+
+        private void MusicPlayerApp_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+
+        private void listBoxSongs_MouseUp(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void listBoxSongs_MouseDown(object sender, MouseEventArgs e)
+        {
+
+        }
+        
+        private void listBoxSongs_PreviewKeyDown(object sender,  PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete)
+            {
+                listBoxSongs.Items.RemoveAt(listBoxSongs.SelectedIndex);
+                
+            }
+            
+        }
+    }
+}
